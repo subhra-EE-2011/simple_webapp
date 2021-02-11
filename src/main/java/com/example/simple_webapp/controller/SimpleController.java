0204eb8a5fa543774f8064feb430ba6be0114791
subhra-EE-2011 <subhra.ee07@gmail.com>
2021-02-11 17:19:22 +0530

    fixed java error

--- src/main/java/com/example/simple_webapp/controller/SimpleController.java
+++ src/main/java/com/example/simple_webapp/controller/SimpleController.java
@@ -1,4 +1,4 @@
-package com.example.simple_webapp.controller
+package com.example.simple_webapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
