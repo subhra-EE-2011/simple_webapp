bc7466c955fb00cb29eb5e53ece32720caee9b23
subhra-EE-2011 <subhra.ee07@gmail.com>
2021-02-11 16:19:51 +0530

    First commit

---
+++ src/main/java/com/example/simple_webapp/SimpleWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/simple_webapp/controller/SimpleController.java
@@ -0,0 +1,13 @@
+package com.example.simple_webapp.controller
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController{
+	
+	@GetMapping(value="/",produces="text/plain")
+	public String welcome(){
+		return "Welcome to simple webapp";
+	}
+}
