ed9d5dd86dd26d6dd9e1831f0614acb3303f0310
subhra-EE-2011 <subhra.ee07@gmail.com>
2021-02-12 09:26:51 +0530

    Changed view message

--- src/main/java/com/example/simple_webapp/controller/SimpleController.java
+++ src/main/java/com/example/simple_webapp/controller/SimpleController.java
@@ -2,7 +2,7 @@ package com.example.simple_webapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.net.InetAddress;
+import java.net.*;
 
 @RestController
 public class SimpleController{
