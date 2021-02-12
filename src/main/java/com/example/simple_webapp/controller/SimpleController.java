dc7b71883db2396d199d838f7a5100f3822a9290
subhra-EE-2011 <subhra.ee07@gmail.com>
2021-02-12 09:02:51 +0530

    Changed view message

--- src/main/java/com/example/simple_webapp/controller/SimpleController.java
+++ src/main/java/com/example/simple_webapp/controller/SimpleController.java
@@ -2,12 +2,23 @@ package com.example.simple_webapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
 
 @RestController
 public class SimpleController{
 	
 	@GetMapping(value="/",produces="text/html")
 	public String welcome(){
-		return "<html><body><h1>Fuck Off</h1></body></html>";
+		
+		String ipAddress = "IP address can't be obtained"
+		
+		try{
+		  InetAddress inetAddress = InetAddress.getLocalHost();
+		  ipAddress = inetAddress.getHostAddress();
+		catch(UnknownHostException e){
+			 e.printStackTrace();
+		 }
+
+		return ipAddress;
 	}
 }
